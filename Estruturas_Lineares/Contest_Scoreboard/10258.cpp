4eea689f5e8fd6d961b05a25fbb863fa26e73097
Guilherme Augusto <guilherme.francais@gmail.com>
2018-04-07 10:17:03 -0300

    Adds 11988 UVA exercise solution

---
+++ Estruturas_Lineares/Contest_Scoreboard/10258.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Result{
+    public:
+        int contestant;
+        int problem;
+        int time;
+        char type;
+};
+
+int main(){
+
+    int tc;
+    string values;
+    scanf("%d", &tc);
+
+    while(tc--){
+
+        // Result result;
+        //
+        // while(scanf("%d %d %d %d", &result.contestant, &result.problem, &result.time, &result.type), ){
+        //
+        // }
+
+
+    }
+
+    return 0;
+}
