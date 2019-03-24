517e2a6bfe1826e6335f33ebafc1a38c322d236d
Guilherme AugustoD <guilherme.francais@gmail.com>
2019-03-24 03:23:56 -0300

    Submit B question of Round 548 contest - WA

---
+++ Codeforces/Contest_Educational_Round_62/b.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    int t;
+    int q;
+    string s;
+
+    cin >> t;
+
+    while(t--){
+        cin >> q;
+        cin >> s;
+
+        
+    }
+
+    return 0;
+}
